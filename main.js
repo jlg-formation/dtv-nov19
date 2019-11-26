a91c97a3987313c7f9087c57cb87a2cffcbfef5e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-26 16:12:05 +0100

    import

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { drawHisto } from "./histo.js";
+
 console.log("start");
 
 const data = [
@@ -19,27 +21,7 @@ const data = [
   }
 ];
 
-const scale = 1000 / Math.max(...data.map(d => d.nbr));
-console.log("scale: ", scale);
-
-const height = 60;
-const margin = 20;
-
-function addRect(parentSvg) {
-  const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-  parentSvg.appendChild(rect);
-  return rect;
-}
-
-data.forEach((d, i) => {
-  // ajoute un rect dans svg
-  // met sa longueur a d.nbr
-  const svg = document.querySelector("svg");
-  const rect = addRect(svg);
-  rect.setAttribute("x", 0);
-  rect.setAttribute("y", 10 + (height + margin) * i);
-  rect.setAttribute("width", Math.floor(scale * d.nbr));
-  rect.setAttribute("height", height);
-});
+const svg = document.querySelector("svg");
+drawHisto(svg, data);
 
 console.log("end");
