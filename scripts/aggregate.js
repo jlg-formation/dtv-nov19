a94ea4e2dc699e07886daa1ea7690ccb7191f89c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-27 09:57:25 +0100

    read csv fron node

--- scripts/aggregate.js
+++ scripts/aggregate.js
@@ -1,6 +1,14 @@
-console.log('start agregate');
+const fs = require("fs");
+const path = require("path");
+const csv = require("fast-csv");
+
+console.log("start agregate");
 
 // open a csv file
+
+fs.createReadStream(path.resolve(__dirname, "../data/small-data.csv"))
+  .pipe(csv.parse({ headers: true }))
+  .on("data", row => console.log(row));
 
 // group
 
