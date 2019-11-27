de368c8dc24b9d48bc9655bb86b8cda638bd10ca
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-27 12:19:08 +0100

    ok

--- scripts/aggregate.js
+++ scripts/aggregate.js
@@ -2,7 +2,22 @@ const fs = require("fs");
 const path = require("path");
 const csv = require("fast-csv");
 
-
+function getCategories() {
+  return new Promise((resolve, reject) => {
+    const categories = [];
+    fs.createReadStream(path.resolve(__dirname, "../data/catv.csv"))
+      .pipe(csv.parse({ headers: true }))
+      .on("data", row => {
+        categories.push(row);
+      })
+      .on("end", () => {
+        resolve(categories);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+}
 
 // open a csv file
 
@@ -12,7 +27,7 @@ fs.createReadStream(path.resolve(__dirname, "../data/data.csv"))
   .pipe(csv.parse({ headers: true }))
   .on("data", row => {
     // group
-    
+
     const group = acc.find(e => e.name === row.catv);
     if (group) {
       group.nbr++;
@@ -23,16 +38,16 @@ fs.createReadStream(path.resolve(__dirname, "../data/data.csv"))
   .on("end", async () => {
     // write a csv file
     try {
-      
-      const formattedCsv = await csv.writeToString(acc, { headers: true });
+      const categories = await getCategories();
+      acc.forEach(d => (d.name = categories.find(x => x.id === d.name).name));
+      const formattedCsv = await csv.writeToString(acc, {
+        headers: true,
+        quoteColumns: [true, false]
+      });
 
-      
       await fs.promises.writeFile(
         path.resolve(__dirname, "../data/group.csv"),
         formattedCsv
       );
-      
-    } catch (e) {
-      
-    }
+    } catch (e) {}
   });
