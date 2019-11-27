04d85e18478ac1dcaed615d1646b02098ca86d4c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-27 11:26:08 +0100

    aggregate ok

--- scripts/aggregate.js
+++ scripts/aggregate.js
@@ -2,14 +2,37 @@ const fs = require("fs");
 const path = require("path");
 const csv = require("fast-csv");
 
-console.log("start agregate");
+
 
 // open a csv file
 
-fs.createReadStream(path.resolve(__dirname, "../data/small-data.csv"))
+const acc = [];
+
+fs.createReadStream(path.resolve(__dirname, "../data/data.csv"))
   .pipe(csv.parse({ headers: true }))
-  .on("data", row => console.log(row));
+  .on("data", row => {
+    // group
+    
+    const group = acc.find(e => e.name === row.catv);
+    if (group) {
+      group.nbr++;
+    } else {
+      acc.push({ name: row.catv, nbr: 1 });
+    }
+  })
+  .on("end", async () => {
+    // write a csv file
+    try {
+      
+      const formattedCsv = await csv.writeToString(acc, { headers: true });
 
-// group
-
-// write a csv file
+      
+      await fs.promises.writeFile(
+        path.resolve(__dirname, "../data/group.csv"),
+        formattedCsv
+      );
+      
+    } catch (e) {
+      
+    }
+  });
