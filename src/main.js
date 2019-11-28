a47b07699432dede427e1e96e05126438f2c3d0a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-28 09:33:13 +0100

    webpack dev server

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { drawHisto } from "./histo-d3.js";
 (async () => {
   try {
     console.log("startxxx");
-    let data = await d3.dsv(",", "../data/group.csv");
+    let data = await d3.dsv(",", "data/group.csv");
     // data = data.sort((a, b) => Math.sign(b.nbr - a.nbr))
     const svg = document.querySelector("svg");
     drawHisto(svg, data);
--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,37 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-  entry: "./src/main.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  devtool: "none",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html"
-    }),
-    new CopyWebpackPlugin([{ from: "src/assets", to: "assets" }])
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        loader: ["style-loader", "css-loader"]
-      }
-    ]
-  }
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+
+  return {
+    entry: "./src/main.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist")
+    },
+    devtool: isProd ? "none" : "inline-source-map",
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html"
+      }),
+      new CopyWebpackPlugin([
+        { from: "src/assets", to: "assets" },
+        { from: "data/group.csv", to: "data/group.csv" }
+      ])
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          loader: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, "dist"),
+      compress: true,
+      port: 9000
+    }
+  };
 };
