2a8842b5ae149fb2076a74619bba48616a0ea63d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-28 11:38:52 +0100

    ok

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,17 @@ import "./style.css";
 import * as d3 from "d3";
 
 import { drawHisto } from "./histo-d3";
+import { IGroup } from "./interfaces/Group";
 
 (async () => {
   try {
     console.log("startxxx");
     const data = await d3.dsv(",", "data/group.csv");
-    const sortedData = data.sort((a, b) => Math.sign(+b.nbr - +a.nbr));
+    const sortedData: any = data.sort((a, b) => Math.sign(+b.nbr - +a.nbr));
     sortedData.length = 10;
-    drawHisto(document.querySelector(".histo-car"), sortedData);
+    drawHisto(document.querySelector(".histo-car"), sortedData as IGroup[]);
     console.log("end");
   } catch (e) {
     console.error(e);
   }
 })();
-
-
