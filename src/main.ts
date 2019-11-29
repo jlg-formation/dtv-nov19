5a24cd41e2de36f1c8c7bd08b7fa56429a0486a5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-11-29 09:07:34 +0100

    bubbles ok

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import "./style.css";
-import * as d3 from "d3";
-import { drawHisto } from "./histo-d3";
 import { BubbleBoard } from "./BubbleBoard";
 
 (async () => {
   try {
     const n = 10;
-    const data = new Array(10).fill(0).map(n => ({
+    const data = new Array(10).fill(0).map(() => ({
       cx: Math.random(),
       cy: Math.random(),
       r: Math.random(),
